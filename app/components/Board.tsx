034eb419d22b404e7d5b4949a536b84e734d9f22
MohdFaisalBidda <biddafaisal@gmail.com>
2023-10-30 01:09:21 +0530

    Column and same column todo dnd done

--- app/components/Board.tsx
+++ app/components/Board.tsx
@@ -6,29 +6,79 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Columns from "./Columns";
 
 function Board() {
-  const [board, getBoard] = useBearStore((state) => [
+  const [board, getBoard, setBoardState] = useBearStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
   console.log(board);
-  
 
-  const handleOnDragEnd = (result:DropResult) =>{
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+    console.log(destination, source, type);
 
-  }
+    if (!destination) return;
+
+    // handle columns dnd
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [remove] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, remove);
+      const modifiedEntries = new Map(entries);
+      setBoardState({ ...board, columns: modifiedEntries });
+    }
+
+    //
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishedColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishedCol: Column = {
+      id: finishedColIndex[0],
+      todos: finishedColIndex[1].todos,
+    };
+
+    if (!startCol || !finishedCol) return;
+
+    if (source.index === destination.index && startCol === finishedCol) return;
+
+    const newTodos = startCol.todos;
+    const [movedTodo] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishedCol.id) {
+      //same column todo moved
+      newTodos.splice(destination.index, 0, movedTodo);
+      const newCol: Column = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      //different column todo moved
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
         {(provided) => (
           <div
-          className={`grid grid-cols-1 md:grid-cols-${Array.from(board.columns.entries()).length} gap-5 max-w-7xl mx-auto mt-10`}
-          {...provided.droppableProps}
-          ref={provided.innerRef}
+            className={`grid grid-cols-1 md:grid-cols-${
+              Array.from(board.columns).length
+            } gap-5 max-w-7xl mx-auto mt-10`}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
           >
             {Array.from(board.columns.entries()).map(([id, column], index) => (
               <Columns id={id} key={id} todos={column.todos} index={index} />
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -2,16 +2,18 @@ import { getTodosGroupedByColumn } from '@/utils/getTodosGroupedByColumn';
 import { create } from 'zustand'
 
 interface BoardStore {
-  board:Board;
-  getBoard:()=>void
+  board: Board;
+  getBoard: () => void;
+  setBoardState: (board: Board) => void;
 }
 
 export const useBearStore = create<BoardStore>((set) => ({
   board: {
-    columns:new Map<TypedColumn,Column>()
+    columns: new Map<TypedColumn, Column>()
   },
-  getBoard:async()=>{
-    const board = await getTodosGroupedByColumn();        
-    set({board});
-  }
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
+  setBoardState: (board) => set({ board })
 }))
